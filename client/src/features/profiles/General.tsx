b08228f9809e015f1b554d69649922408f6d84e9
Hassan Omar <100947729+HasanOmarHasan@users.noreply.github.com>
2025-06-22 16:23:05 +0300

    Update General.tsx

--- client/src/features/profiles/General.tsx
+++ client/src/features/profiles/General.tsx
@@ -1,13 +1,18 @@
-import InputItem from '../../ui/InputItem'
-import Button from '../../ui/Button'
+// import InputItem from '../../ui/InputItem'
+import Button from "../../ui/Button";
+import { useAuth } from "../../context/Auth";
 
 export default function General() {
+  const { user, darkMode, toggleTheme } = useAuth();
+  console.log(user, darkMode);
+
   return (
-      <div className=' py-6  flex flex-col gap-6 justify-center items-center '>
-          <h1 className='text-3xl font-semibold'>General</h1>
-          <InputItem name='theme' select='Change theme' value='Dark' value2='Light' />
-          <InputItem name='Language' select='Change Language' value='Arabic' value2='English' />
-          <Button content='soon ...' disabled  />
+    <div className=" py-6  flex flex-col gap-6 justify-center items-center ">
+      <h1 className="text-3xl font-semibold">General</h1>
+
+      <h3>Toggle Theme</h3>
+      <Button content={darkMode ? "Dark" : "Light"} onClick={toggleTheme} />
+      
     </div>
-  )
+  );
 }
