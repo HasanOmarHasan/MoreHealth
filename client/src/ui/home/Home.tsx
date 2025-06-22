b4e3aaa78d46a6e95e908fd6ba1771516c815827
Hassan Omar <100947729+HasanOmarHasan@users.noreply.github.com>
2025-06-22 16:20:51 +0300

    Update Home.tsx

--- client/src/ui/home/Home.tsx
+++ client/src/ui/home/Home.tsx
@@ -4,15 +4,21 @@ import FQA from "./FQA";
 import Hero from "./Hero";
 
 export default function Home() {
+  const container = "mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12";
   return (
-    <div className="">
-      <Hero />
-      <ContentSection />
+    <div>
+      <div className={container}>
+        <Hero />
+      </div>
 
-      <Testimonials />
+      <div className="bg-gray-100  px-4 sm:px-6 lg:px-8 py-12">
+        <ContentSection />
+      </div>
 
-      <FQA/> 
-     
+      <div className={container}>
+        <Testimonials />
+        <FQA />
+      </div>
     </div>
   );
 }
