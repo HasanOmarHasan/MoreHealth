37d0581a576aea7f0642545ed9296264da457e31
Hassan Omar <100947729+HasanOmarHasan@users.noreply.github.com>
2025-07-04 22:26:25 +0300

    Update config.ts

--- client/src/features/AI Services/config.ts
+++ client/src/features/AI Services/config.ts
@@ -1,2 +1,2 @@
-export const ai_chat_URL : string = "https://a52af58532af3b26e1.gradio.live"; // or leave it empty for testing
-export const Symptom_URL : string = "https://8d1ba8668f3edcd40d.gradio.live/"; // or leave it empty for testing
+export const ai_chat_URL : string = ""; // or leave it empty for testing
+export const Symptom_URL : string = ""; // or leave it empty for testing
